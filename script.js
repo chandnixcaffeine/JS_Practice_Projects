71f7fdcb0694afcca162b11258ef936e8b9e5a70
Chandni <thechandni5@gmail.com>
2024-06-09 14:26:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const date = new Date()
-
-const day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-const months = ["January","February","April","May","June","July","Auguast","September","October","November","December"]
-
-document.querySelector("#date").innerHTML = date.getDate()
-document.querySelector("#day").innerHTML = day[date.getDay()]
-document.querySelector("#month").innerHTML = months[date.getMonth()]
-document.querySelector("#year").innerHTML = date.getFullYear()
